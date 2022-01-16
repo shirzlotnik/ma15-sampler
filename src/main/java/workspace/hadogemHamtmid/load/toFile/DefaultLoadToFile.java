7030b7b2334d2736604a370d6d6620a21c720b29
shirzlotnik <shirzlotnik@gmail.com>
2022-01-16 18:07:06 +0200

    added loadToJson class

--- src/main/java/workspace/hadogemHamtmid/load/toFile/DefaultLoadToFile.java
+++ src/main/java/workspace/hadogemHamtmid/load/toFile/DefaultLoadToFile.java
@@ -2,7 +2,6 @@ package workspace.hadogemHamtmid.load.toFile;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import workspace.hadogemHamtmid.load.directoryManager.FindAvailableFile;
 import workspace.hadogemHamtmid.load.toFile.abstraction.LoadToFile;
 
 @NoArgsConstructor
