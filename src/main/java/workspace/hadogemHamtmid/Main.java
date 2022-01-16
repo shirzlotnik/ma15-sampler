6406406be8c68cb86a292d73993185cdc883bcda
shirzlotnik <shirzlotnik@gmail.com>
2022-01-16 18:18:18 +0200

    read properties in main

--- src/main/java/workspace/hadogemHamtmid/Main.java
+++ src/main/java/workspace/hadogemHamtmid/Main.java
@@ -1,7 +1,13 @@
 package workspace.hadogemHamtmid;
 
+import java.util.Properties;
+
 public class Main {
     public static void main (String[] args) {
+        ReadProperties rp = new ReadProperties("config.properties").getInstance();
+        final String JSON_DIRECTORY_PATH = rp.getProperty("JSON_DIRECTORY_PATH");
+        final String CSV_FILE_PATH = rp.getProperty("CSV_FILE_PATH");
+        final int MAX_OBJECTS = Integer.parseInt(rp.getProperty("MAX_OBJECTS"));
 
     }
 }
