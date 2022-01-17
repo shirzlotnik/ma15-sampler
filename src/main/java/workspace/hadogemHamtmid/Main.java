59958a050c4acfb5a97bf0af297f7ca01a599f59
shirzlotnik <shirzlotnik@gmail.com>
2022-01-17 08:10:29 +0200

    added manager class for part A

--- src/main/java/workspace/hadogemHamtmid/Main.java
+++ src/main/java/workspace/hadogemHamtmid/Main.java
@@ -10,18 +10,6 @@ import java.util.Properties;
 
 public class Main {
     public static void main (String[] args) {
-        ReadProperties rp = new ReadProperties("/Users/shirzlotnik/javaProjects/hadogem_hamtmid/ma15-sampler/src/main/resources/config.properties");
-        final String JSON_DIRECTORY_PATH = rp.getProperty("JSON_DIRECTORY_PATH");
-        final String CSV_FILE_PATH = rp.getProperty("CSV_FILE_PATH");
-        final int MAX_OBJECTS = Integer.parseInt(rp.getProperty("MAX_OBJECTS"));
-
-        ExtractFromCsv efCSV = new ExtractFromCsv();
-        List<MadaReport> reports = efCSV.extract(CSV_FILE_PATH);
-        LoadToJson ltj = new LoadToJson(MAX_OBJECTS);
-        try {
-            ltj.load(JSON_DIRECTORY_PATH, reports);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        PartA A = new PartA();
     }
 }
