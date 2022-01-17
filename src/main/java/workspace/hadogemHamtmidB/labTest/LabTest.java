fa8958c63edc2fd0c2d8cf95c9721e98d21762c2
shirzlotnik <shirzlotnik@gmail.com>
2022-01-17 08:59:16 +0200

    added labTests class

---
+++ src/main/java/workspace/hadogemHamtmidB/labTest/LabTest.java
@@ -0,0 +1,23 @@
+package workspace.hadogemHamtmidB.labTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class LabTest {
+    String IDNum;
+    String IDType;
+    String FirstName;
+    String LastName;
+    String ResultDate;
+    String BirthDate;
+    String LabCode;
+    String StickerNumber;
+    String ResultTestCorona;
+    String Variant;
+    String TestType;
+
+}
