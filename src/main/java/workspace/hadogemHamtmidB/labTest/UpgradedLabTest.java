87d8406126ba290f5f9eb3be4d758b81f7c50568
shirzlotnik <shirzlotnik@gmail.com>
2022-01-17 09:01:53 +0200

    added upgradedlabTests class inckued joindDate, healthCardId and name

---
+++ src/main/java/workspace/hadogemHamtmidB/labTest/UpgradedLabTest.java
@@ -0,0 +1,27 @@
+package workspace.hadogemHamtmidB.labTest;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class UpgradedLabTest {
+    String IDNum;
+    String IDType;
+    String FirstName;
+    String LastName;
+    String ResultDate;
+    String BirthDate;
+    String LabCode;
+    String StickerNumber;
+    String ResultTestCorona;
+    String Variant;
+    String TestType;
+    String JoinDate;
+    String HealthCardID;
+    String HealthCardName;
+
+}
