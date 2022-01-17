c9b07579494badde79072d020fc7dca980f036b3
shirzlotnik <shirzlotnik@gmail.com>
2022-01-17 18:22:39 +0200

    Update PartB.java

--- src/main/java/workspace/hadogemHamtmid/partB/PartB.java
+++ src/main/java/workspace/hadogemHamtmid/partB/PartB.java
@@ -1,15 +1,12 @@
 package workspace.hadogemHamtmid.partB;
 
-import workspace.hadogemHamtmid.CSVExtractManager;
+import workspace.hadogemHamtmid.extract.CSVExtractManager;
 import workspace.hadogemHamtmid.LoadManager;
 import workspace.hadogemHamtmid.ReadProperties;
 import workspace.hadogemHamtmid.partA.extract.abstraction.DefaultExtractionFromFile;
 import workspace.hadogemHamtmid.partA.load.DefaultLoadToFile;
-import workspace.hadogemHamtmid.partB.extract.ExtractLabTestsFromCSV;
 import workspace.hadogemHamtmid.partB.labTest.LabTest;
-import workspace.hadogemHamtmid.partB.load.LoadToXML;
 
-import java.io.IOException;
 import java.util.List;
 
 public class PartB {
