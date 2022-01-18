1f8d17bdc6521035642e39cecdc22973eda59a49
shirzlotnik <shirzlotnik@gmail.com>
2022-01-18 17:09:21 +0200

    added Serology class and reader from csv

--- src/main/java/workspace/hadogemHamtmid/PartD/PartD.java
+++ src/main/java/workspace/hadogemHamtmid/PartD/PartD.java
@@ -3,13 +3,8 @@ package workspace.hadogemHamtmid.PartD;
 import java.util.HashMap;
 
 public class PartD {
-    HashMap<String, Integer> kitMap;
 
     public PartD() {
-        this.kitMap = new HashMap<String, Integer>() {{
-            put("15", 500);
-            put("16", 1000);
-            put("17", 1500);
-        }};
+
     }
 }
--- src/main/java/workspace/hadogemHamtmid/extract/CSVExtractManager.java
+++ src/main/java/workspace/hadogemHamtmid/extract/CSVExtractManager.java
@@ -1,5 +1,6 @@
 package workspace.hadogemHamtmid.extract;
 
+import workspace.hadogemHamtmid.PartD.extract.ExtractSerologyFromCsv;
 import workspace.hadogemHamtmid.partA.extract.ExtractMadaReportsFromCsv;
 import workspace.hadogemHamtmid.partA.extract.abstraction.DefaultExtractionFromFile;
 import workspace.hadogemHamtmid.partB.extract.ExtractLabTests;
@@ -10,9 +11,15 @@ public class CSVExtractManager extends DefaultExtractManager{
     HashMap<String, DefaultExtractionFromFile> objectAndExtractor;
 
     public CSVExtractManager() {
+        HashMap<String,Integer> kitMap = new HashMap<String, Integer>() {{
+            put("15", 500);
+            put("16", 1000);
+            put("17", 1500);
+        }};
         this.objectAndExtractor = new HashMap<String, DefaultExtractionFromFile>() {{
             put("MadaReport", new ExtractMadaReportsFromCsv());
             put("LabTest", new ExtractLabTests());
+            put("Serology", new ExtractSerologyFromCsv(kitMap));
         }};
     }
 
