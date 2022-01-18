24cdf8f214b53a39940d27e25cb5d31c1fd63e40
shirzlotnik <shirzlotnik@gmail.com>
2022-01-18 18:03:31 +0200

    fixed size Menegment for part C

--- src/main/java/workspace/hadogemHamtmid/PartC/crossInfo/LoadCrossedDataToJson.java
+++ src/main/java/workspace/hadogemHamtmid/PartC/crossInfo/LoadCrossedDataToJson.java
@@ -8,6 +8,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -29,15 +31,21 @@ public class LoadCrossedDataToJson<T> extends DefaultLoadToFile<T> {
         int objectCount = maxObjects;
         List<T> fixedSizeList = new LinkedList<>();
         T c = null;
+        long maxBytes = 20000000;
+        this.filePath = String.format("%s%s%d.json", directoryPath, "/positive_corona_people", this.fileCount);
+        this.fileCount++;
         try {
             while (iterator.hasNext()) {
-                if (objectCount >= maxObjects) {
+                File jsonFile = new File(this.filePath);
+                if (Files.size(Paths.get(this.filePath)) < maxBytes) {
                     writeToFile(directoryPath, fixedSizeList);
-                    objectCount = 0;
                     fixedSizeList = new LinkedList<>();
-                } if (iterator != null) {
+                } else {
+                    this.filePath = String.format("%s%s%d.json", directoryPath, "/positive_corona_people", this.fileCount);
+                    this.fileCount++;
+                }
+                if (iterator != null) {
                     c = iterator.next();
-                    objectCount++;
                     fixedSizeList.add(c);
                 }
             }
@@ -53,16 +61,16 @@ public class LoadCrossedDataToJson<T> extends DefaultLoadToFile<T> {
         if (fixedSizeList.size() == 0) {
             return;
         }
-        this.filePath = String.format("%s%s%d.json", directoryPath, "/positive_corona_people", this.fileCount);
         File jsonFile = new File(this.filePath);
         if (!jsonFile.exists()) {
             jsonFile.createNewFile();
         }
-        BufferedWriter writer = new BufferedWriter(new FileWriter(jsonFile));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(jsonFile, true));
         if (fixedSizeList == null) {
             fixedSizeList = new LinkedList<>();
+            return;
         }
-        String json = this.mapper.writeValueAsString(fixedSizeList);
+        String json = this.mapper.writeValueAsString(fixedSizeList.get(0));
         writer.write(json);
         this.fileCount++;
         writer.flush();
